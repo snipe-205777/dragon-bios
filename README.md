890488a4e523ecbf72c9aa6f31ac9f820cfe782d
snipe-205777 <glasselephant0@gmail.com>
2023-08-08 20:45:25 +0100

    create flags to only run for a single dragon

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ This repo contains:
 - Each dragon's lore and other bio settings
 - A script to compile these contents with respect to a template
 - The final coded bio created from the script.
+
+## How to run
+
+Use `python create_bio.py` to run the script and create completed bios for all dragons in the spreadsheet.
+
+To only create a bio for a single dragon, add the `-d` or `--dragon` flags to the command, followed by the name of the dragon. The name is case-sensitive. With the flag the command should read `python create_bio.py -d <dragon_name>` or `python create_bio.py --dragon <dragon_name>` (without angle brackets).
--- create_bio.py
+++ create_bio.py
@@ -1,3 +1,4 @@
+import argparse
 import pandas as pd
 from bio_blocks.credits import credits
 from bio_blocks.dividers import dividers
@@ -7,10 +8,29 @@ dragon_data = pd.read_excel("bio_info.xlsx", sheet_name="dragons", header=0)
 
 dragon_data = dragon_data.fillna(0)
 
-for i in range(dragon_data.shape[0] - 1):
-    dragon = dragon_data.loc[i]
-
+def create_bio(dragon):
     divider = dividers(dragon)
  
     header = create_header(dragon)
     credit = credits(dragon)
+
+    print(dragon["name"])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dragon", help="the dragon to create a bio for")
+    args = parser.parse_args()
+
+    if args.dragon:
+        match_name = dragon_data[dragon_data["name"] == args.dragon]
+        if match_name.empty:
+            raise ValueError("No dragon found with this name. This argument are case-sensitive")
+        else:
+            row = match_name.index[0]
+            dragon = dragon_data.loc[row]
+            create_bio(dragon)
+    else:
+        for i in range(dragon_data.shape[0] - 1):
+            dragon = dragon_data.loc[i]
+            create_bio(dragon)
