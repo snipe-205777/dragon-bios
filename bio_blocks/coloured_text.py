e5d649cda63a834fcfc494c9e87b92e880f52bba
snipe-205777 <glasselephant0@gmail.com>
2023-08-08 17:30:40 +0100

    set colour for coloured text based on flight

---
+++ bio_blocks/coloured_text.py
@@ -0,0 +1,9 @@
+from resources.text_colour import text_colour
+
+def coloured_text(dragon):
+    if dragon["divider_type"]:
+        divider_type = dragon["divider_type"]
+    else:
+        divider_type = "Swirls"
+
+    return text_colour[divider_type][dragon["flight"]]
