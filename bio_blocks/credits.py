77748a1ba9384ad775e8ae88b27ba6c55744e149
snipe-205777 <glasselephant0@gmail.com>
2023-08-09 20:52:21 +0100

    add bloodsport crown credit

--- bio_blocks/credits.py
+++ bio_blocks/credits.py
@@ -6,8 +6,9 @@ def credits(dragon):
     icon_type = dragon["icon_type"] if dragon["icon_type"] else "Runes"
     divider_type = dragon["divider_type"] if dragon["divider_type"] else "Swirls"
     pride_icons = "\nPride icons: Snipe 205777" if create_flags(dragon) else ""
+    bloodsport_crown = "\nHunger Games crown: Aevios 191020" if dragon["bloodsport_victories"] else ""
 
     return f"""[right][size=1]Credits:
 Bio template: Snipe 205777
 Flight icon: {flight_icons[icon_type]["Credit"]}
-Flight dividers: {section_dividers[divider_type]["Credit"]}{pride_icons}[/size][/right]"""
+Flight dividers: {section_dividers[divider_type]["Credit"]}{pride_icons}{bloodsport_crown}[/size][/right]"""
