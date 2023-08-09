1591e4aaf6c9bf0495ed4761fc1f2d03e167339a
snipe-205777 <glasselephant0@gmail.com>
2023-08-09 20:44:09 +0100

    add template credit

--- bio_blocks/credits.py
+++ bio_blocks/credits.py
@@ -8,5 +8,6 @@ def credits(dragon):
     pride_icons = "\nPride icons: Snipe 205777" if create_flags(dragon) else ""
 
     return f"""[right][size=1]Credits:
+Bio template: Snipe 205777
 Flight icon: {flight_icons[icon_type]["Credit"]}
 Flight dividers: {section_dividers[divider_type]["Credit"]}{pride_icons}[/size][/right]"""
