701408a7a84023a98bd3a66537f36c0b39568ee1
snipe-205777 <140005305+snipe-205777@users.noreply.github.com>
2024-08-12 21:43:15 +0100

    compile bbcode for a section

--- scripts/badge_coder.py
+++ scripts/badge_coder.py
@@ -10,6 +10,24 @@ def code_badge(badge_name):
     return f'[center][size=1]{badge["Pre-image"]}[url={badge["Link"]}][img alt="{badge["Alt Text"]}"]{badge["Image"]}[/img][/url]\n{badge["Caption"]}'
 
 
+def code_section(section):
+    section_header = f'[size=2][u]{section["header"]}[/u][/size]'
+    section_badges = []
+
+    for row in section["badges"]:
+        row_badges = []
+
+        for badge in row:
+            row_badges.append(code_badge(badge))
+
+        coded_row = "[nextcol]".join(row_badges)
+        section_badges.append(coded_row)
+
+    joint_badges = "[/columns][br][br][columns]".join(section_badges)
+    section = f"{section_header}[columns]{joint_badges}[/columns]"
+    return section.replace("[br]", "\n")
+
+
 def compile_badges():
     pass
 
