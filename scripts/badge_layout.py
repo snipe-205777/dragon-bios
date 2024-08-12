b547e350173d96f329c010a005a649bb0a6af1a6
snipe-205777 <140005305+snipe-205777@users.noreply.github.com>
2024-08-12 22:38:45 +0100

    add complete badge layout

--- scripts/badge_layout.py
+++ scripts/badge_layout.py
@@ -3,7 +3,34 @@ badge_layout = [
         "header": "Arcane Dom",
         "badges": [
             ["Snowmageddon", "Quest Push 1", "Quest Push 2", "RPG", "Spirit Week 1"],
-            ["Spirit Week 2", "Spirit Week 3", "Spirit Week 4", "Spirit Week 5"]
+            ["Spirit Week 2", "Spirit Week 3", "Spirit Week 4", "Spirit Week 5"],
+            ["HC 16 Prevels", "HC 16 Sprite Quests", "HC 16 Overall Apoc", "HC 16 SS Apoc", "HC 16 SS Champs"],
+            ["Stress Test Jul 16", "Stress Test Aug 16", "Starfall 16", "Whodunnit", "Steampunk'd"],
+            ["House Holiday Party", "BOTB3 Prelevels Apoc", "BOTB3 Prelevels Dormer", "BOTB3 Prelevels Profit"],
+            ["BOTB3 Apoc", "Love is in the Air", "Stellaflora Participation", "Stellaflora Apoc", "Space Exploration"],
+            ["Coffee Conq", "Stellaflora II Participation", "Stellaflora II Apoc", "Father's Day", "Frantic Finals HC"],
+            ["AvS HC Participation", "AvS HC OOFR", "AvS House Cup 17", "Starfall 17", "Spreadsheet Witch"],
+            ["Bake Sale", "Tea Profit", "Eyes of March", "Spellunking", "AvP Cryptids"],
+            ["Spa Day", "Ball of Sports Participation", "Ball of Sports SS 4", "Ball of Sports Apoc", "Space Diner"],
+            ["SFC 18 Attendant Sun", "SFC 18 Attendant Mon", "SFC 18 Attendant Tue", "SFC 18 Attendant Wed", "SFC 18 Attendant Thu"],
+            ["SFC 18 Attendant Fri", "SFC 18 Attendant Sat", "Starfall 18", "Spells and Spooks", "Gingerbread"],
+            ["Hatchie Naptime", "Familiar Ball", "March-ing Band", "Red Carpet", "Starlit Ceremony"],
+            ["Bae's Bday", "AvN Attendant", "AvN Blitzes", "AvN House Pride", "AvN"],
+            ["Arts and Crafts", "Starfall 19", "Tricky Treats", "Harvest Celebration", "Snowmageddon EPW"],
+            ["Aurora Sorealis", "Not Love Potions", "Eyes of March 2", "Gardening", "Spring Cleaning"],
+            ["Summer Solstice", "Yearbook Starseeker", "Yearbook Dreamstrider", "Yearbook Shatterspell", "Yearbook Runesworn"],
+            ["Yearbook All Sprite", "Yearbook Extra", "Yearbook Prelevel", "Yearbook", "Starfall 20"],
+            ["Crystal Rush", "Haunted Starwood", "Holiday Party", "Snowdrop Soiree", "Level Up"],
+            ["April Flowers", "Maren Expedition", "HC 21 Prelevels", "HC 21 SQ SK", "HC 21 SQ DS"],
+            ["HC 21 SQ SH", "HC 21 SQ RS", "HC 21 Pride Quests", "HC 21", "Starfall 21"],
+            ["Gourd Guard Graveyard", "Safety First", "Silly Sweater", "Souper Bowl", "Sleepover"],
+            ["Arcadian Delight", "Spring Cleaning 22", "Clean Springing", "HC Field Trip", "Starfall 22"],
+            ["Enchanted Library", "Upgraded Arcadia", "Gaoler Supply Run", "Aurora Dance", "April Floods"],
+            ["May Buds", "Supernova Celebration", "Astronomy School", "Astronomy School Apoc", "Pirates of Arr-cane"],
+            ["AvL", "AvL Prelevel Arcane", "AvL Prelevel Earth", "AvL Prelevel Fire", "AvL Prelevel Wind"],
+            ["AvL Prelevel Water", "AvL Prelevel Shadow", "AvL Prelevel Ice", "AvL Prelevel Lightning", "AvL Prelevel Light"],
+            ["AvL Prelevel Nature", "AvL Prelevel Plague", "AvL Prelevel Rainbow", "Gone Fishing", "Astrolodome Food Festival"],
+            ["Aetherversary", "Solar Eclipse", "Sprite Quests Jul 24", "Meteorite Mayhem"]
         ]
     },
     {
@@ -11,6 +38,77 @@ badge_layout = [
         "badges": [
             ["BHG Reaver", "BHG MVP Profit Aug 16", "BHG MVP Stress Aug 16", "BHG Elite", "BHG Titan"],
             ["BHG Standard Immortal", "BHG Intense Centurion"]
+        ]
+    },
+    {
+        "header": "Bonl",
+        "badges": [
+            ["FvW Donation", "House Cup Donation", "FvL Donation", "AvS Donation Comp", "FGB Stretch Goal 1"],
+            ["FGB Stretch Goal 2", "FGB Stretch Goal 3", "Celestial Donation", "Bank Donation Total", "Bank Donation Weeks"],
+            ["September 18 Bank Goal", "October 18 Bank Goal", "IvL Donation", "200kt SvF", "SSG March 19 2"],
+            ["SSG March 19 3", "SSG March 19", "Celestial May 19", "Donation Mar 20", "Bank June 21"],
+            ["Bank July 21", "Donation Comp Aug 21", "Bank Sep 21", "Gone Fishing Donation", "Aetherversary Donation"],
+            ["Spring Clean-Out", "Meteorite Donation"]
+        ]
+    },
+    {
+        "header": "Supporting",
+        "badges": [
+            ["Shadow Mother's Day", "Wind Father's Day 16", "Thundercrack 16", "Wind vS Sun", "Wind vS Wed"],
+            ["Wind vS Thu", "PvW Sun/Mon", "PvW Wed/Fri", "Noodle Fest", "RoR 16"],
+            ["WvF Water", "WvF Fire", "RBC Light", "Plague of Frogs", "Trickmurk 17"],
+            ["FvL Lightning Dorming", "FvL Fire", "FvL Lightning", "MistJam 17", "Greenskeeper's 17", "Brightshine 17"],
+            ["IvW Ice", "IvW Ice Day 1", "Flameforger's 17", "WvL Lightning Dorming", "WvL Wind"],
+            ["LvE Earth Dorming Arcane", "LvE Earth Dorming Fire", "LvE Earth Dorming Ice", "LvE Earth Dorming Wind", "LvE Earth Dorming Light"],
+            ["LvE Earth Dorming Water", "LvE Earth Dorming Nature", "LvE Earth Dorming Lightning", "LvE Earth Dorming Shadow", "LvE Earth Dorming Plague"],
+            ["Gala 18", "Valentine Wind", "Shadow Persistent", "Inferno Fire"],
+            ["MistJam 18", "ETNW", "EvN Nature", "EvN Earth", "Earth Persistent"],
+            ["Wavecrest 18", "Mothers Day 18", "Star Wars", "Star Wars Wed", "Greenskeeper's 18"],
+            ["Father's Day 18", "Brightshine 18", "WvW Water", "WvW Daily", "Wind WvW Merc"],
+            ["Seasons Sun", "Seasons Fri", "Seasons Sat", "Flameforger's 18", "Shadow Oct 18 Mon"],
+            ["Shadow Oct 18 Tue", "Shadow Oct 18 Wed", "Shadow Oct 18 Thu", "Shadow Oct 18 Fri", "Shadow Oct 18 Sat"],
+            ["RoR 18 Participation", "RoR 18 Sun", "RoR 18 Mon", "RoR 18 Tue", "RoR 18 Wed"],
+            ["RoR 18 Thu", "RoR 18 Fri", "RoR 18 Sat", "RoR 18 Levels", "Water Oct 18 Sun"],
+            ["Water Oct 18 Mon", "Water Oct 18 Tue", "Water Oct 18 Wed", "Water Oct 18 Thu", "IvL Lightning Board Arcane", "IvL Lightning Board Fire"],
+            ["IvL Lightning Board Plague", "IvL Lightning Board Water", "IvL Lightning Board Wind", "IvL Lightning Board Shadow", "IvL Lightning Board Earth", "IvL Lightning Board Nature"],
+            ["IvL Lightning Board Ice", "IvL Lightning Board Light", "IvL Snowboarding Arcane", "IvL Snowboarding Ice", "IvL Snowboarding Fire"],
+            ["IvL Snowboarding Plague", "IvL Snowboarding Nature", "IvL Snowboarding Shadow", "IvL Snowboarding Water", "IvL Snowboarding Wind"],
+            ["IvL Snowboarding Lightning", "IvL Snowboarding Earth", "IvL Snowboarding Light", "IvL Lightning Sun", "IvL Lightning Mon"],
+            ["IvL Lightning Tue", "IvL Lightning Wed", "IvL Lightning Thu", "IvL Lightning Fri", "IvL Lightning Sat"],
+            ["IvL Lightning Levels", "IvL Ice Sun", "IvL Ice Mon", "IvL Ice Tue", "IvL Ice Wed"],
+            ["IvL Ice Thu", "IvL Ice Fri", "IvL Ice Sat", "IvL Ice Levels", "Merc IvL"],
+            ["SvF Shadow Sun", "SvF Fire Mercing", "Nature Meme Participation", "Gala 19"],
+            ["Gala 19 Sun", "Gala 19 Mon", "Gala 19 Tue", "Gala 19 Wed", "Gala 19 Thu"],
+            ["Gala 19 Fri", "Gala 19 Sat", "Valightine's 19", "Trickmurk 19 Sun", "Trickmurk 19 Mon"],
+            ["Trickmurk 19 Tue", "Trickmurk 19 Wed", "Trickmurk 19 Thu", "Trickmurk 19 Fri", "Trickmurk 19 Sat"],
+            ["MistJam 19 Sun", "MistJam 19 Mon", "MistJam 19 Tue", "MistJam 19 Wed", "MistJam 19 Thu"],
+            ["MistJam 19 Fri", "MistJam 19 Sat", "Lightning Lab Sun", "Lightning Lab Mon", "Lightning Lab Sat"],
+            ["Ice Cat Cafe Sun", "Ice Cat Cafe Mon", "Ice Cat Cafe Tue", "Ice Cat Cafe Wed", "Ice Cat Cafe Fri"],
+            ["Ice Cat Cafe Sat", "WaveSat 19 Sun", "WaveSat 19 Sat", "WaveSat 19 Levels", "Earth Day 19 Sun", "Earth Day 19 Mon"],
+            ["Earth Day 19 Tue", "Earth Day 19 Wed", "Earth Day 19 Fri", "Earth Day 19 Sat", "Shadow Books Sun"],
+            ["Shadow Books Mon", "Shadow Books Tue", "Shadow Books Thu", "Shadow Books Fri", "Shadow Books Sat"],
+            ["GG 19 Participation", "GG 19 Sun", "GG 19 Mon", "GG 19 Tue"],
+            ["GG 19 Levels", "GG 19 Wed", "GG 19 Thu", "GG 19 Fri"],
+            ["GG 19 Sat", "BSJ 19 Sun", "BSJ 19 Mon", "BSJ 19 Tue", "BSJ 19 Wed"],
+            ["BSJ 19 Thu", "BSJ 19 Fri", "BSJ 19 Sat", "Plague Jun 19 Level", "Plague Jun 19 Daily"],
+            ["IvW2 Ice Board A", "IvW2 Water Board A", "IvW2 Water Board E", "IvW2 Water Board F", "IvW2 Water Board I"],
+            ["IvW2 Water Board L", "IvW2 Water Board Ln", "IvW2 Water Board N", "IvW2 Water Board P", "IvW2 Water Board S"],
+            ["IvW2 Water Board Wn", "IvW2 Water Board Wt", "Valightine's 20", "FvN 20 Saturday", "FvN 20 Levels"],
+            ["GG 20 Fri", "GG 20 Levels", "WvP Wind Dorm Earth", "WvP Wind Dorm Fire", "WvP Wind Dorm Wind"],
+            ["WvP Wind Dorm Water", "WvP Wind Dorm Shadow", "WvP Wind Dorm Ice", "WvP Wind Dorm Lightning", "WvP Wind Dorm Light"],
+            ["WvP Wind Dorm Nature", "WvP Wind Dorm Plague", "WvP Wind Dorm Arcane", "WaveSat 21 Sat", "GG 21 Levels"],
+            ["Brightshine 21", "Shadow SvI", "Ice SvI Levels", "Ice SvI Sat", "WvL Wind Mercs"],
+            ["Heartfelt Delivery", "Everbloom Resort Sat", "Lightning Apr 24 Sun", "Lightning Apr 24 Mon", "Lightning Apr 24 Tue"],
+            ["Lightning Apr 24 Wed", "Lightning Apr 24 Thu", "Lightning Apr 24 Fri", "Lightning Apr 24 Sat", "Meteorite Nature Sat"],
+            ["Meteorite Nature Levels"]
+        ]
+    },
+    {
+        "header": "Community",
+        "badges": [
+            ["Shadow Meteor", "Wind Meteor", "Meteorbomb Myth Static", "Water Meteor"],
+            ["Nature Meteor", "Light Meteor", "Lightning Meteor", "Fire Meteor"],
+            ["Plague Meteor", "Inktober 16", "Scribbler Reader"]
         ]
     }
 ]
