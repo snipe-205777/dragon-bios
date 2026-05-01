6a5d83d083bd001e884d50c86a63cd51d519ff31
snipe-205777 <140005305+snipe-205777@users.noreply.github.com>
2026-05-01 11:31:12 +0100

    change relationships block

--- utils/bio_blocks/relationships.py
+++ utils/bio_blocks/relationships.py
@@ -1,36 +1,25 @@
 from math import ceil
 import pandas as pd
+from utils.bio_blocks.coloured_text import coloured_text
 
 
-def get_relations(dragon):
+def relationships_block(dragon):
     relations = pd.read_csv("relationships.csv", header=0, skipinitialspace=True)
     relations = relations.fillna("")
     relations = relations[relations["dragon"] == dragon["name"]]
 
     if relations.empty:
-        return []
+        return ""
 
     relations["url"] = relations.apply(lambda row: f"[url=/dragon/{row.relation_id}]", axis=1)
     relations["portrait_url"] = relations.apply(lambda row: f"/rendern/portraits/{ceil((row.relation_id+1)/100)}/{row.relation_id}p.png", axis=1)
     relations["image"] = relations.apply(lambda row: f"[item={row.familiar_type}]" if row.familiar_type else f'{row.url}[img alt="{row.relation_name} avatar"]{row.portrait_url}[/img][/url]', axis=1)
-    relations["code"] = relations.apply(lambda row: f"[center]{row.image}\n[size=2]{row.relation_name}\n[i]{row.relationship}[/i][/size]", axis=1)
+    relations["code"] = relations.apply(lambda row: f"[columns]{row.image}[nextcol]\n\n{row.relation_name}\n[i]{row.relationship}[/i]\n{row.description}[/columns]", axis=1)
 
-    return list(relations["code"])
+    text_colour = coloured_text(dragon)
+    
+    block = f"""[center][color={text_colour}][size=4]Relationships[/size][/color][/center]
 
+{"\n".join(relations["code"])}"""
 
-def relationships_block(dragon):
-    relations = get_relations(dragon)
-
-    transparent_placeholder = {
-        2: "https://i.postimg.cc/66Msz3P9/2-avatars.png",
-        3: "https://i.postimg.cc/xj6rgCZj/3-avatars.png",
-        4: "https://i.postimg.cc/jqMVv51S/4-avatars.png"
-    }
-
-    if len(relations) > 1:
-        placeholder = transparent_placeholder[len(relations)]
-        return f'[columns][img alt="transparent placeholder"]{placeholder}[/img][nextcol]{"[nextcol]".join(relations)}[/columns]'
-    elif len(relations) == 1:
-        return f"{relations[0]}[/center]"
-    else:
-        return ""
+    return block
